0d1fe1cf4c24040047447f95a5fc502c2c02ab6c
Jesus rodriguez <cristi542012@gmail.com>
2026-07-14 20:15:44 -0500

    Entregable Modulo 4: ALIADOS, CLIENTES Y TELEMEDICINA

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	// apiUrl: '/api/',
-	apiUrl: 'https://7gjtn29n-3001.use2.devtunnels.ms/api/',
+	apiUrl: '/api/',
+	// apiUrl: 'https://7gjtn29n-3001.use2.devtunnels.ms/api/',
 };
